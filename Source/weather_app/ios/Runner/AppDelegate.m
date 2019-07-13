dcfe7ff03f99742992694e39b8ce2d5d4856bd3a
Michael <mejevin@gmail.com>
2019-07-13 03:08:53 +0300

    Fix?

--- Source/weather_app/ios/Runner/AppDelegate.m
+++ Source/weather_app/ios/Runner/AppDelegate.m
@@ -8,8 +8,8 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
   [FIRApp configure];
+  [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
